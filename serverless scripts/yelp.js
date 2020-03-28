857bbdca3e51c30c3430803d598bc2a547ff8a46
Adrian Darian <adarian@ucmerced.edu>
2020-03-28 13:57:25 -0700

    Fully functional yelp query and initial suggestion algo

--- serverless scripts/yelp.js
+++ serverless scripts/yelp.js
@@ -1,11 +1,38 @@
+const axios = require("axios");
+const cors = require("cors")({ origin: true });
+
 /**
  * Responds to any HTTP request.
  *
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send(message);
-  };
-  
+exports.yelpBusinessData = (req, res) => {
+  cors(req, res, () => {
+    if (req.method !== "GET") {
+      return res.status(401).json({
+        message: "Not allowed"
+      });
+    }
+
+    return axios
+      .get("https://api.yelp.com/v3/businesses/search", {
+        params: {
+          term: req.query.term,
+          latitude: req.query.latitude,
+          longitude: req.query.longitude
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`
+        }
+      })
+      .then(response => {
+        return res.status(200).json(response.data);
+      })
+      .catch(err => {
+        return res.status(500).json({
+          error: err
+        });
+      });
+  });
+};
